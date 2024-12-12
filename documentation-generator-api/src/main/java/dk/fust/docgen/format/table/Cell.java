35730ee4d3157c0774128b06a89ff5846d711635
Patrick Fust <pfu@nine.dk>
2024-12-12 10:31:13 +0100

    Removed unused rowSpan and added handling content as long

--- documentation-generator-api/src/main/java/dk/fust/docgen/format/table/Cell.java
+++ documentation-generator-api/src/main/java/dk/fust/docgen/format/table/Cell.java
@@ -1,6 +1,7 @@
 package dk.fust.docgen.format.table;
 
 import lombok.Data;
+import lombok.Setter;
 
 /**
  * Represent a cell in a table
@@ -9,9 +10,10 @@ import lombok.Data;
 public class Cell {
 
     private int colspan = 1;
-    private int rowspan = 1;
     private boolean header = false;
+    @Setter
     private String content;
+    private Long contentLong;
 
     /**
      * Default constructor
@@ -22,13 +24,11 @@ public class Cell {
     /**
      * Constructor
      * @param colspan colspan
-     * @param rowspan rowspan
      * @param content content
      * @param header true if it's a header
      */
-    public Cell(int colspan, int rowspan, String content, boolean header) {
+    public Cell(int colspan, String content, boolean header) {
         this.colspan = colspan;
-        this.rowspan = rowspan;
         this.header = header;
         this.content = content;
     }
@@ -36,12 +36,10 @@ public class Cell {
     /**
      * Constructor
      * @param colspan colspan
-     * @param rowspan rowspan
      * @param content content
      */
-    public Cell(int colspan, int rowspan, String content) {
+    public Cell(int colspan, String content) {
         this.colspan = colspan;
-        this.rowspan = rowspan;
         this.content = content;
     }
 
@@ -52,4 +50,35 @@ public class Cell {
     public Cell(String content) {
         this.content = content;
     }
+
+    /**
+     * Constructor using a long
+     * @param contentLong content as long
+     */
+    public Cell(Long contentLong) {
+        this.contentLong = contentLong;
+    }
+
+    /**
+     * Constructor
+     * @param content content
+     * @param header true if it's a header
+     */
+    public Cell(String content, boolean header) {
+        this.content = content;
+        this.header = header;
+    }
+
+    /**
+     * Returning a String representation of contentLong if not null,
+     * otherwise content
+     * @return contentLong or content
+     */
+    public String getContent() {
+        if (contentLong != null) {
+            return Long.toString(contentLong);
+        }
+        return content;
+    }
+
 }
