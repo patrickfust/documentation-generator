05a74b3a42e031e1bca11f52c5ef2ec297340398
Patrick Fust <pfu@nine.dk>
2025-01-30 10:25:00 +0100

    Added comment on index

--- documentation-generator-api/src/main/java/dk/fust/docgen/model/Index.java
+++ documentation-generator-api/src/main/java/dk/fust/docgen/model/Index.java
@@ -13,6 +13,9 @@ public class Index {
     @Description("Name of the index")
     private String name;
 
+    @Description("Comment to be added to the index")
+    private String comment;
+
     private boolean unique = false;
 
     @Description(value = "List of fields for the index", required = true)
