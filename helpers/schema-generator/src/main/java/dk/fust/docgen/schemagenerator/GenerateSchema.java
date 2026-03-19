079af542df926ea20d1fb3e375be1003ab05cf19
Patrick Fust <patrick@fust.dk>
2026-03-19 12:29:52 +0100

    chore(deps): update dependency jsonschema-generator to 5.0.0

--- helpers/schema-generator/src/main/java/dk/fust/docgen/schemagenerator/GenerateSchema.java
+++ helpers/schema-generator/src/main/java/dk/fust/docgen/schemagenerator/GenerateSchema.java
@@ -1,8 +1,8 @@
 package dk.fust.docgen.schemagenerator;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.node.ArrayNode;
+import tools.jackson.databind.node.ObjectNode;
 import com.github.victools.jsonschema.generator.CustomPropertyDefinition;
 import com.github.victools.jsonschema.generator.CustomPropertyDefinitionProvider;
 import com.github.victools.jsonschema.generator.FieldScope;
